bc42cb64cb6fd1c18df01800f90f2bf80fedc85b
Vuongdao21 <vuongds2111@gmail.com>
2025-06-09 23:27:31 +0700

    Nộp bài tập 8

---
+++ 69_2251172563_DAOSYVUONG_BAITAP8/BaiTap8/src/baitap8/Mediator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package baitap8;
+
+/**
+ *
+ * @author Vuong Dao
+ */
+public interface Mediator {
+    boolean requestWithdraw(Account account, double amount);
+}
+
